202c9fcbb0a716f9a0477403f4c97a0b57767d99
Kevin Connor <kevinpaulconnor@gmail.com>
2016-10-18 11:22:19 -0700

    use local stylesheets
    …and remove unused stylesheet decisionmaking code

--- source/javascripts/getdata.js
+++ source/javascripts/getdata.js
@@ -13,7 +13,7 @@
   	for ( var i = 0; i < data.length; i++ ) {
   		input[i] = findUrlinObject( data[i] );
   	}
-    console.log(input);
+    //console.log(input);
   	embed(input);
   }
 
@@ -36,39 +36,13 @@
   	return result;
   }
 
-  function addJS() {
-  	quizType = $('input[name="quiz-type"]:checked').val();
-		if (quizType == 'quiz') {
-			$('body').append('<script src="s/quiz.js" type="text/javascript"><\/script>');
-		} else if (quizType == 'flowchart') {
-			$('body').append('<script src="s/flowchart.js" type="text/javascript"><\/script>');
-		} else {
-      $('body').append('<script src="s/binary.js" type="text/javascript"><\/script>');
-    }
-  }
-
   function submitquiz() {
   		$('.quiz-container').empty();
   		buildflowchart();
   }
 
-  function getStylesheet() {
-  	pub = $('input[name="pub"]:checked').val();
-    $('body').append('<script type="text/javascript">var pub ="' + pub + '"</script>');
-    pubStylesheet = "s/quiz-" + pub + ".css";
-    // pubStylesheet = "/stylesheets/quiz-" + pub + ".css";
-  }
-
   function embed(input) {
-    $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write(\"&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>\");&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "'; var pub = '" + pub + "'; &lt;/script>&lt;script src='http://assets.sbnation.com.s3.amazonaws.com/features/quiz-generator/" + quizType + ".js'>&lt;/script>");
-    // $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write('&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>');&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = '" + pubStylesheet + "';&lt;/script>&lt;script src='/javascripts/" + quizType + ".js'>&lt;/script>");
-    //addJS();
-  }
-
-  function buildquiz(){
-    url = $('#url').val();
-    init();
-    getStylesheet();
+    $("#embedcode").html("&lt;div class='quiz-container'></div>&lt;script type='text/javascript'>window.jQuery || document.write(\"&lt;script src='//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>&lt;&#92;/script>\");&lt;/script>&lt;script type='text/javascript'>var input = " + JSON.stringify(input) + "; var pubStylesheet = 's/quiz-vox.css'; var pub = 'vox'; &lt;/script>&lt;script src='s/flowchart.js'>&lt;/script>");
   }
 
   function googleSpreadsheetHack(url) {
@@ -85,7 +59,6 @@
   function buildflowchart() {
     url = googleSpreadsheetHack($('#url').val());
     init();
-    getStylesheet();
   }
 
   $(document).ready(function() {
