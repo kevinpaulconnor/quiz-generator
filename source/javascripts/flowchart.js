efa9e57ed9a4beaf33d22505e613fe8e5fe1a913
Kevin Connor <kevinpaulconnor@gmail.com>
2016-11-02 11:42:10 -0700

    no mobile hover effect

--- source/javascripts/flowchart.js
+++ source/javascripts/flowchart.js
@@ -189,5 +189,9 @@
   $(document).ready(function(){
     trackEvent('loaded', 'Quiz is loaded');
     unpackQuizHack();
+    var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+		if (!touchsupport){ // browser doesn't support touch
+    	document.documentElement.className += " non-touch"
+		}
   });
 })(jQuery);
